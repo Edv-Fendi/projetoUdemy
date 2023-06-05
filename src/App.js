2e3ef12fbff34f86f4e232bac6665b300caccef9
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2023-06-04 21:37:02 -0300

    props

--- src/App.js
+++ src/App.js
@@ -1,44 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react'
+import { PostCard } from './components/PostCard';
 
 class App extends Component {
   state = {
-    name: 'Edvaldo Fendi',
-    counter: 0
+    posts: [],
   }
 
+  timeOutUpdate = null;
 
-  handlePClick = () => {
-    this.setState({ name: 'Bruh' });
+  handleTimeOut = () => {
+    const { posts } = this.state
+    posts[0].title = "O titulo ta diferente"
+    setTimeout(() => {
+      this.setState({ posts, counter: Date() })
+    }, 1000);
+  }
+  componentDidMount() {
+    this.loadPosts()
   }
 
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state
-    this.setState({ counter: counter + 1 });
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
+
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse])
+
+    const postsJson = await posts.json()
+    const photosJson = await photos.json()
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    })
+
+    this.setState({ posts: postsAndPhotos })
   }
 
   render() {
-    const { name, counter } = this.state
+    const { posts } = this.state
+
     return (
-      <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={this.handleAClick}
-          >
-            Clique para somar mais um
-          </a>
-        </header>
-      </div>
+      <section className='container'>
+        <div className="posts" >
+          {posts.map(post => (
+            <PostCard
+              key={post.id}
+              title={post.title}
+              body={post.body}
+              id={post.id}
+              cover={post.cover}
+            />
+
+          ))
+          }
+        </div>
+      </section >
     );
   }
 }
